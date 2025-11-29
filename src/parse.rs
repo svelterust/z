105bba8fef655d0ae1dce5f485009c337d755ede
SvelteRust <oddharald@myhren.ai>
2025-11-29 10:05:32 +0100

    Basic parser

--- src/parse.rs
+++ src/parse.rs
@@ -1,11 +1,21 @@
 use nom::{
     IResult, Parser,
+    branch::alt,
     bytes::{complete::tag, take_until},
-    character::complete::{alpha1, digit1},
+    character::complete::{alpha1, digit1, multispace0},
     combinator::{map, opt, recognize},
-    number::double,
-    sequence::{delimited, pair},
+    error::ParseError,
+    multi::many0,
+    number::complete::double,
+    sequence::{delimited, preceded, separated_pair},
 };
+
+pub fn ws<'a, O, E: ParseError<&'a str>, F>(inner: F) -> impl Parser<&'a str, Output = O, Error = E>
+where
+    F: Parser<&'a str, Output = O, Error = E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
 
 enum Atom {
     Symbol(String),
@@ -14,8 +24,12 @@ enum Atom {
     Float(f64),
 }
 
-fn parse_name(input: &str) -> IResult<&str, Atom> {
-    map(alpha1, |it: &str| Atom::Symbol(it.to_string())).parse(input)
+fn parse_name(input: &str) -> IResult<&str, String> {
+    map(alpha1, str::to_string).parse(input)
+}
+
+fn parse_symbol(input: &str) -> IResult<&str, Atom> {
+    map(parse_name, Atom::Symbol).parse(input)
 }
 
 fn parse_string(input: &str) -> IResult<&str, Atom> {
@@ -24,10 +38,70 @@ fn parse_string(input: &str) -> IResult<&str, Atom> {
 }
 
 fn parse_number(input: &str) -> IResult<&str, Atom> {
-    let parser = recognize(pair(opt(tag("-")), digit1));
+    let parser = recognize((opt(tag("-")), digit1));
     map(parser, |it: &str| Atom::Number(it.parse().unwrap())).parse(input)
 }
 
 fn parse_float(input: &str) -> IResult<&str, Atom> {
     map(double, |it: f64| Atom::Float(it)).parse(input)
+}
+
+fn parse_atom(input: &str) -> IResult<&str, Atom> {
+    alt((parse_symbol, parse_string, parse_number, parse_float)).parse(input)
+}
+
+enum Expr {
+    Constant(Atom),
+    Declare {
+        name: String,
+        value: Box<Expr>,
+    },
+    Call {
+        name: String,
+        value: Box<Expr>,
+    },
+    Function {
+        name: String,
+        arg: Box<Expr>,
+        body: Vec<Expr>,
+    },
+}
+
+fn parse_constant(input: &str) -> IResult<&str, Expr> {
+    map(parse_atom, Expr::Constant).parse(input)
+}
+
+fn parse_declare(input: &str) -> IResult<&str, Expr> {
+    let parser = separated_pair(parse_name, ws(tag(":=")), parse_constant);
+    map(parser, |(name, value)| Expr::Declare {
+        name,
+        value: Box::new(value),
+    })
+    .parse(input)
+}
+
+fn parse_call(input: &str) -> IResult<&str, Expr> {
+    let parse_arg = delimited(tag("("), parse_constant, tag(")"));
+    let parser = (parse_name, parse_arg);
+    map(parser, |(name, arg)| Expr::Call {
+        name,
+        value: Box::new(arg),
+    })
+    .parse(input)
+}
+
+fn parse_function(input: &str) -> IResult<&str, Expr> {
+    let parse_arg = delimited(tag("("), parse_constant, tag(")"));
+    let parse_body = delimited(tag("{"), ws(many0(parse_expr)), tag("}"));
+    let parser = preceded(tag("fn"), (ws(parse_name), parse_arg, ws(parse_body)));
+    map(parser, |(name, arg, body)| Expr::Function {
+        name,
+        arg: Box::new(arg),
+        body,
+    })
+    .parse(input)
+}
+
+fn parse_expr(input: &str) -> IResult<&str, Expr> {
+    alt((parse_function, parse_declare, parse_call, parse_constant)).parse(input)
 }
