e9a6a4c0c764f3577784259cbb674d9654000749
SvelteRust <oddharald@myhren.ai>
2025-11-29 10:50:04 +0100

    QBE + TCC working, now parse -> qbe -> tcc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use qbe::{DataDef, DataItem, Function, Instr, Linkage, Module, Type, Value};
 use z::Result;
 
 enum Command {
@@ -33,6 +34,29 @@ impl Args {
     }
 }
 
+fn generate_main(module: &mut Module) {
+    let mut func = Function::new(Linkage::public(), "main", Vec::new(), Some(Type::Word));
+
+    func.add_block("start");
+    func.add_instr(Instr::Call(
+        "printf".into(),
+        vec![(Type::Long, Value::Global("name".into()))],
+        None,
+    ));
+    func.add_instr(Instr::Ret(Some(Value::Const(0))));
+
+    module.add_function(func);
+}
+
+fn generate_name(module: &mut Module) {
+    let items = vec![
+        (Type::Byte, DataItem::Str("Odd-Harald".into())),
+        (Type::Byte, DataItem::Const(0)),
+    ];
+    let data = DataDef::new(Linkage::private(), "name", None, items);
+    module.add_data(data);
+}
+
 fn main() -> Result<()> {
     // Get args
     let Ok(args) = Args::new() else {
@@ -44,5 +68,12 @@ fn main() -> Result<()> {
     let input = std::fs::read_to_string(args.file)?;
     let ast = z::parse::parse_ast(&input)?;
     println!("Got AST: {ast:#?}");
+
+    // QBE
+    let mut module = Module::new();
+    generate_main(&mut module);
+    generate_name(&mut module);
+    println!("{module}");
+    std::fs::write("output.ssa", module.to_string())?;
     Ok(())
 }
